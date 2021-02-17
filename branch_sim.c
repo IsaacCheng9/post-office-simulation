e107e107db0834d149e45d371b62866497374240
Isaac Cheng <isaaccheng9@gmail.com>
2021-02-17 23:54:09 +0000

    Add arguments to main

--- branch_sim.c
+++ branch_sim.c
@@ -29,36 +29,43 @@ int dequeue(QUEUE *);
 void print_queue(QUEUE *);
 
 /* Main function */
-int main()
+int main(int argc, char **argv)
 {
     /* Creates a random number of service points. */
     srand(time(0));
     /* int points = rand() / 500; */
-    int points = 20;
-    int *service_points = (int *)create_service_points(points);
+    int num_points = 20;
+    int *service_points = (int *)create_service_points(num_points);
 
     /* Displays all the service points at the start. */
-    printf("Number of Service Points: %d\n", points);
+    printf("Number of Service Points: %d\n", num_points);
     int j;
-    for (j = 0; j < points; j++)
+    for (j = 0; j < num_points; j++)
     {
         printf("Service Point: %d, Value: %d\n", j, service_points[j]);
     }
 
+    // Starts the simulation.
+    int num_customers = atoi(argv[1]);
+    int max_size = atoi(argv[2]);
+    int simulation_length = atoi(argv[3]);
+    int time_slice;
     /* Creates the queue of customers. */
-    int max_size = 20;
+    for (time_slice = 0; time_slice < simulation_length; time_slice++)
+    {
+        printf("Success!\n");
+    }
+
+    
     QUEUE *q = initialise(max_size);
-    int k;
-    for (k = 0; k < 20; k++)
+
+    /* int k;
+    for (k = 0; k < num_customers; k++)
     {
         enqueue(q, k);
     }
     printf("Queue Front: %d\n", q->front->hours);
-    printf("Queue Rear: %d\n", q->rear->hours);
-    print_queue(q);
-
-    dequeue(q);
-    print_queue(q);
+    printf("Queue Rear: %d\n", q->rear->hours); */
 
     free(service_points);
     free(q);
@@ -68,21 +75,21 @@ int main()
 /* Other functions */
 
 /* Creates the empty service points to start with. */
-int *create_service_points(int points)
+int *create_service_points(int num_points)
 {
-    int i;
+    int point;
     /* Dynamically allocates memory based on the number of service points. */
     int *service_points = NULL;
-    if (!(service_points = (int *)malloc(points * sizeof(int))))
+    if (!(service_points = (int *)malloc(num_points * sizeof(int))))
     {
         fprintf(stderr, "Insufficient memory.\n");
         exit(EXIT_FAILURE);
     };
 
     /* Assigns each service point to represent them being empty. */
-    for (i = 0; i < points; i++)
+    for (point = 0; point < num_points; point++)
     {
-        service_points[i] = 0;
+        service_points[point] = 0;
     }
 
     return service_points;
